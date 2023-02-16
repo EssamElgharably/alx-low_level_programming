f097edf56e39bd673fcfaf3fae0c193769edb294
EssamElgharably <esam.elgharably@gmail.com>
2023-02-16 15:07:45 -0800

    Task #6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+Size of a char: sizeof(char);
+Size of an int: sizeof(int);
+Size of a long int: sizeof(long int);
+Size of a long long int: sizeof(long long int);
+Size of a float: sizeof(float);
+}
