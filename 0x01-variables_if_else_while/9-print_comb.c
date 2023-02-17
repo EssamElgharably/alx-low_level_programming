3197cd86c4e131b2249d8d2a5a16731f9e434218
EssamElgharably <esam.elgharably@gmail.com>
2023-02-17 13:50:44 -0800

    Task #9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a = '0';
+
+	while (a <= '9')
+	{
+		if (a != 0)
+		{
+			putchar(a);
+			a++;
+			putchar(44);
+			putchar(32);
+		}
+		else
+		{
+			putchar(a)
+			a++;
+		}
+	}
+	return (0);
+}
