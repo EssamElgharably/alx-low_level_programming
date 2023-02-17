4c726a39c04a5ffea7c4f726e9b54c1c65d58b3d
EssamElgharably <esam.elgharably@gmail.com>
2023-02-17 11:54:15 -0800

    Task #2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a = 97;
+	
+	while (a <= 122)
+	{
+		putchar(a);
+		a++;
+	}
+	return (0);
+}
