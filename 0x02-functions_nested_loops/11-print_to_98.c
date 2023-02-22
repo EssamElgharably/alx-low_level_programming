53d14e44fe7f230d5056f31071deaf3387153b1a
EssamElgharably <esam.elgharably@gmail.com>
2023-02-21 17:56:31 -0800

    Task #11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * print_to_98 - prints the sequence till it reach 98
+ * @n: starting number of the sequence
+*/
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	else if (n == 98)
+	{
+		printf("%d", n);
+	}
+	else
+	{
+		for (j = n; j <= 98; j++)
+		{
+			printf("%d", j);
+			if (j != 98)
+			{
+				putchar(',');
+				putchar(',');
+			}
+		}
+	}
+}
