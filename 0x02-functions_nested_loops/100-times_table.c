72038fb29d5292dba9d8e6da48ca495693c68cb9
EssamElgharably <esam.elgharably@gmail.com>
2023-02-21 19:11:42 -0800

    Task #12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,50 +2,44 @@
 /**
  * print_times_table - function that prints the n times table, starting with 0
  * @n: max
- * Return: if n > 15 or n < 0
 */
 void print_times_table(int n)
 {
 	int i, j, t;
 
-	if (n > 15 || n < 0)
+	if (n <= 15 && n >= 0)
 	{
-		return;
-	}
-	else
-	{
-		if (n == 0)
+		for (i = 0; i <= n; i++)
 		{
-			_putchar('0');
-			_putchar('\n');
-		}
-		else
-		{
-			for (i = 0; i <= n; i++)
+			for (j = 0; j <= n; j++)
 			{
-			_putchar('0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-				for (j = 1; j <= n; j++)
+				t = i * j;
+				if (j == 0)
 				{
-					t = i * j;
-					if ((t / 10) > 0)
-					{
-						_putchar((t / 10) + '0');
-					}
-					else
-					{
-						_putchar(' ');
-					}
+					_putchar(k + '0');
+				} else if (t < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(t + '0');
+				} else if (t > 10 && t < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((t / 10) + '0');
 					_putchar((t % 10) + '0');
-					if (j != n)
-					{
-						_putchar(',');
-					}
+				} else if (t >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((t / 100) + '0');
+					_putchar(((t / 10) % 10) + '0');
+					_putchar((t % 10) + '0');
 				}
-			_putchar('\n');
 			}
+		_putchar('\n');
 		}
-	}
 }
